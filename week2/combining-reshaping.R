42015ef33731e9f6bea03863d316901a0757d76d
Nava Nizard <nava.nizard@gmail.com>
2026-06-01 16:34:57 -0400

    Uploaded part of Day 5 work

--- week2/combining-reshaping.R
+++ week2/combining-reshaping.R
@@ -4,29 +4,46 @@ library(tidyverse)
 # 12.2.1. Exercise 2
 # Compute the rate for table2, and table 4a + table4b. You will need
 # to perform four operations:
+
+#TABLE 2:
 #   1. Extract the number of TB cases per country per year.
-table2 %>%
-  pivot_wider(names_from = type, values_from = count) %>% 
-  group_by(country, year) %>%
-  select(cases)
+    # table2 %>%
+    #   pivot_wider(names_from = type, values_from = count) %>% 
+    #   group_by(country, year) %>%
+    #   select(cases)
 #   2. Extract the matching population per country per year.
-table2 %>%
-  pivot_wider(names_from = type, values_from = count) %>% 
-  group_by(country, year) %>%
-  select(population)
+    # table2 %>%
+    #   pivot_wider(names_from = type, values_from = count) %>% 
+    #   group_by(country, year) %>%
+    #   select(population)
 #   3. Divide cases by population, and multiply by 10000.
+#   4. Store back in the appropriate place.
 table2 %>%
   pivot_wider(names_from = type, values_from = count) %>% 
   group_by(country, year) %>%
-  mutate(ratio = (cases / population)* 10000)
+  mutate(ratio_of_cases_to_pop = (cases / population)* 10000)
+
+
+#TABLE 4:
+#Join the tables
+tbl4a <- table4a %>% pivot_longer(names_to = "year", values_to = "cases", cols = c('1999', '2000'))
+tbl4b <- table4b %>% pivot_longer(names_to = "year", values_to = "population", cols = c('1999', '2000'))
+table4 <- full_join(tbl4a, tbl4b)
+#   1. Extract the number of TB cases per country per year.
+    # table4 %>%
+    #   select(country, year, cases)
+#   2. Extract the matching population per country per year.
+    # table4 %>%
+    #   select(country, year, population)
+#   3. Divide cases by population, and multiply by 10000.
 #   4. Store back in the appropriate place.
-
-
-
+table4 %>%
+  mutate(ratio_of_cases_to_pop = (cases / population) * 10000)
 
 # Which representation is easiest to work with? Which is hardest? Why?
-# Add your answer as a comment.
-
+# ANSWER: The table4 representation is easier, since it had (once joined), the cases and population
+#         counts as seperate columns so it was easier to select / extract the data needed. With table2, 
+#         we first had to restructure the data (pivot_wider()) in order to seperate the fields.
 
 ####################################################################################
 # 12.3.3 Exercise 1
@@ -43,13 +60,21 @@ stocks %>%
 
 # (Hint: look at the variable types and think about column names.)
 # pivot_longer() has a names_ptypes argument, e.g.  names_ptypes = list(year = double()). 
-# What does it do? Add your answer as a comment.
+# What does it do? 
+
+# ANSWER: pivot_wider() creates a new column, with the name of the new column being of type "char", so even if
+#         we were to pivot_longer() it back, which would take the names of the columns and stores them as values, 
+#         it would now store those as characters, regardless of their initial types.
 
 ####################################################################################
 # 12.3.3 Exercise 3
 # What would happen if you widen this table? Why? 
+# ANSWER: It would attempt to seperate the ages and heights as new columns, but since
+#         there are duplicate entries the age for the same given name, there'd be an error. 
 # How could you add a new column to uniquely identify each value?
-#  Add your answers as a comment.
+# ANSWER: pivot_wider(names_from: names, values_from = values) after adding a row number to distinguish
+#         between each observation. 
+
 people <- tribble(
   ~name,             ~names,  ~values,
   #-----------------|--------|------
