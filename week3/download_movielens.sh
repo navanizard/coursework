754d22d26cfc5119ca168ee05f413e9dccff2eab
Nava Nizard <nava.nizard@gmail.com>
2026-06-10 15:30:33 -0400

    Finished movie lens assignment

--- week3/download_movielens.sh
+++ week3/download_movielens.sh
@@ -5,10 +5,10 @@
 # see http://grouplens.org/datasets/movielens/ for more info
 #
 
-url=http://files.grouplens.org/datasets/movielens/ml-10m.zip
+url=https://files.grouplens.org/datasets/movielens/ml-10m.zip
 
 # download ratings zip file
-[ -f ml-10M.zip ] || curl -o -L ml-10M.zip $url
+[ -f ml-10M.zip ] || curl -L -o ml-10M.zip $url
 
 # uncompress zip file
 if [ ! -f ratings.dat ]
