e61994efca5aaa9b55a946c15df9ad6935984493
Nava Nizard <nava.nizard@gmail.com>
2026-06-11 11:56:13 -0400

    Completed Part A of ngrams

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -4,5 +4,8 @@
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
 
-cat googlebooks-eng-all-totalcounts-20120701.txt |
+#The file starts with a leading space and tab, and ends with a tab, so need
+#to remove them before converting tabs to newlines to avoid empty lines.
+
+cat googlebooks-eng-all-totalcounts-20120701.txt | sed 's/^ \t//;s/\t$//' | 
     tr '\t' '\n' > total_counts.csv
